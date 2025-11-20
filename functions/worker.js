0e963e307345a087b10a8a97a1b66a095fbd472f
zx211 <90975704+zx211@users.noreply.github.com>
2025-11-20 23:38:45 +0800

    Update worker.js

--- functions/worker.js
+++ functions/worker.js
@@ -1,24 +1,49 @@
-// functions/worker.js
-// 豆瓣图片代理 Worker
+export default {
+    async fetch(request) {
+        try {
+            const url = new URL(request.url);
+            const target = url.searchParams.get("url");
 
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request))
-})
+            if (!target) {
+                return new Response("缺少 url 参数", { status: 400 });
+            }
 
-async function handleRequest(request) {
-  const url = new URL(request.url)
-  const target = url.searchParams.get("url")
-  
-  if (!target) {
-    return new Response("请提供 ?url= 目标图片地址", { status: 400 })
-  }
+            // 仅允许现存的豆瓣图片域名
+            const doubanHosts = [
+                "img1.doubanio.com",
+                "img2.doubanio.com",
+                "img3.doubanio.com",
+                "img9.doubanio.com",
+                "img1.douban.com",
+                "img2.douban.com",
+                "img3.douban.com",
+                "img9.douban.com"
+            ];
 
-  try {
-    const res = await fetch(target)
-    const headers = new Headers(res.headers)
-    headers.set("Access-Control-Allow-Origin", "*") // 允许跨域
-    return new Response(res.body, { status: res.status, headers })
-  } catch (err) {
-    return new Response("请求失败：" + err, { status: 500 })
-  }
-}
+            const host = new URL(target).hostname;
+
+            if (!doubanHosts.includes(host)) {
+                return new Response("禁止访问非豆瓣图片域名", { status: 403 });
+            }
+
+            // 请求真实豆瓣图片
+            const res = await fetch(target, {
+                headers: {
+                    "Referer": "https://www.douban.com",
+                    "User-Agent": "Mozilla/5.0"
+                }
+            });
+
+            const newHeaders = new Headers(res.headers);
+            newHeaders.set("Access-Control-Allow-Origin", "*");
+
+            return new Response(res.body, {
+                status: res.status,
+                headers: newHeaders
+            });
+
+        } catch (err) {
+            return new Response("Worker 错误: " + err.toString(), { status: 500 });
+        }
+    }
+};
