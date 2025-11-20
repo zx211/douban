d0d5e7fadcc36003c76af207020fdc8d1fa11802
zx211 <90975704+zx211@users.noreply.github.com>
2025-11-20 18:31:33 +0800

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -11,7 +11,7 @@ async function handleRequest(request) {
   try {
     const res = await fetch(target, {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0",
         "Referer": "https://movie.douban.com/"
       }
     })
