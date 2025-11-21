25b460fe590415e29d9ffb66c285264100c1d00d
zx211 <90975704+zx211@users.noreply.github.com>
2025-11-21 15:15:04 +0800

    Refactor worker.js to remove hardcoded IP and clean up
    Removed the hardcoded allowed IP and unused code.

--- functions/worker.js
+++ functions/worker.js
@@ -6,7 +6,7 @@ const NODES = [
 ];
 
 // 你的服务器 IP，只有这个 IP 可以访问
-const ALLOWED_IP = '134.122.153.28';
+//const ALLOWED_IP = '你的服务器IP';
 
 export default {
   async fetch(request) {
@@ -50,25 +50,6 @@ export default {
         return new Response(body, { status: 200, headers });
       } catch (e) {
         // 节点失败尝试下一个
-        continue;
-      }
-    }
-
-    return new Response('所有节点请求失败', { status: 502 });
-  }
-};          }
-        };
-
-        const res = await fetch(newUrl, cfOptions);
-        if (!res.ok) continue;
-
-        // 设置缓存
-        const headers = new Headers(res.headers);
-        headers.set('Cache-Control', 'public, max-age=31536000'); // 1年缓存
-
-        const body = await res.arrayBuffer();
-        return new Response(body, { status: 200, headers });
-      } catch (e) {
         continue;
       }
     }
